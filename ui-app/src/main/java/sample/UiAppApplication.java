26f06db9fc8b6f2a18c1e9a616b687625a0cf13e
Joe Grandja <jgrandja@pivotal.io>
2019-05-14 21:20:38 -0400

    Enable OIDC

--- ui-app/src/main/java/sample/UiAppApplication.java
+++ ui-app/src/main/java/sample/UiAppApplication.java
@@ -17,15 +17,13 @@ package sample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.oauth2.client.servlet.OAuth2ClientAutoConfiguration;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import sample.config.ServicesConfig;
 
 /**
  * @author Joe Grandja
  */
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class, OAuth2ClientAutoConfiguration.class})
+@SpringBootApplication
 @EnableConfigurationProperties(ServicesConfig.class)
 public class UiAppApplication {
 
